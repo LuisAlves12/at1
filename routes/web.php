1944da0192b52fb57fd3c86383923604bff4bcd7
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-12 17:26:59 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/toni', function () {
     return view('welcome');
 });
+
+Route::get('/bem-vindo', function (){
+    echo('Olá Mundo');
+});
+
+Route::get('/', function (){
+    echo('<h1> Olá mundo!</h1>');
+});
+
+Route::get('/bem-vindo/aedah',function(){
+    echo('<h1> Esta é só para o Agrupamento :) </h1>');
+});
+
+Route::get('/nome/{nome}',function($nome){
+    echo('<h1> Olá ' .$nome. ' </h1>');
+});
+
+Route::get('/nome/{nome}/{apelido}',function($nome,$apelido){
+   echo('<h1> Olá '.$nome.' '.$apelido.'</h1>');
+});
+
+Route::get('/{nome}/{numero}', function($nome,$numero){
+    $numero=is_numeric($numero)?$numero:5;
+    for($i=0; $i<$numero; $i++){
+        echo($nome. '<br> ');
+    }
+});
+
+           
+           
+           
+           
+           
+           
