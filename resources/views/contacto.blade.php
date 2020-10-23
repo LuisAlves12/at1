90483d91c699268f4ff952a32e9a9960e5014a46
luisa <luisa@DESKTOP-O1O6AUM.local>
2020-10-23 18:16:09 +0100

    Continuação dos apontamentos do Modulo 18

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -6,18 +6,19 @@ Contactos
 Fale connosco
 @endsection
 @section('conteudo')
-    <form method="post" action="{{route('mostrar.form')}}">
-    <label for="nome">Nome</label>
-    <input type="text" name="nome">
-    <label for="morada">Morada</label>
-    <input type="text" name="morada">
-    <label for="automovel">Automóvel</label>
-    <select name="automovel">
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="mercedes">Mercedes</option>
-        <option value="Audi">Audi</option>
-    </select>
-    <button type="submit">enviar</button>
-    </form>
+<form method="post" action="{{route('processar.form')}}">
+    @csrf
+<label for="nome">Nome</label>
+<input type="text" name="nome">
+<label for="morada">Morada</label>
+<input type="text" name="morada">
+<label for="automovel">Automovel</label>
+<select name="automovel">
+    <option value="Volvo">Volvo</option>
+    <option value="Saab">Saab</option>
+    <option value="Mercedes">Mercedes</option>
+    <option value="Audi">Audi</option>
+</select>
+<button type="submit">Enviar</button>
+</form>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,6 +3,8 @@
         <head>
             <meta charset="utf-8"/>
             <title>@yield('titulo-pagina')</title>
+            <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+            <link rel="stylesheet" href="{{asset('css/all.min.css')}}"> 
         <head>
         <body>
             <h1>A minha web APP em laravel</h1>
@@ -11,6 +13,9 @@
             <div class="menu-rodape">
                 <h2> Opções de navegação</h2>
                 <a href="{{route('equipas.index')}}">Inicio</a>
-                <a href="{{route('equipas.lista')}}"><i class="far fa-trash">Ver Equipas</i></a>
+                <a href="{{route('equipas.lista')}}"><i class="fas fa-trash"></i></a>
+            </div>
+            <script src="{{asset('js/bootstrap.min.js')}}"></script>
+            <script src="{{asset('js/all.min.js')}}"></script>
         </body>
     </html>
